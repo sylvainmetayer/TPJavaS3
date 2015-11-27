c19f875edfdfac530d6f58426d3ec26a28416686
Sylvain Metayer <sylvain_metayer@outlook.fr>
2015-11-27 17:29:38 +0100

    Continuité exo9

--- TD3/src/exo9/TestExecutorService.java
+++ TD3/src/exo9/TestExecutorService.java
@@ -3,9 +3,10 @@
  */
 package exo9;
 
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author VAREILLE-METAYER
@@ -15,18 +16,21 @@ import java.util.concurrent.Executors;
  */
 public class TestExecutorService {
 
-
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		ExecutorService exec = Executors.newFixedThreadPool(2);
-		Thread t1 = new Thread(new Task());
-		Thread t2 = new Thread(new Task());
-		Thread t3 = new Thread(new Task());
-		exec.submit(t1);
-		exec.submit(t2);
-		exec.submit(t3);
+
+		exec.submit(new Task());
+		exec.submit(new Task());
+		exec.submit(new Task());
+		// Si l'on soumet 3 tâche, la troisième tache sera lancée que lorsque un
+		// tache est finie, et que donc un emplacement dans le pool de Thread
+		// est disponible
+
+		exec.shutdown(); // on utilise
+
 	}
 
 }
